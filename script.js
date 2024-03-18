b563f6c22aa537785d63bc04ab38ef9065681aae
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-03-18 13:01:30 +0100

    Add return statement to the functions

--- script.js
+++ script.js
@@ -1,13 +1,78 @@
-const possibleChoises = ['rock', 'paper', 'scissors']
-const computerChoise = getComputerChoice()
-const playerchoice = getPlayerChoice()
+const possibleChoices = ['rock', 'paper', 'scissors']
+const computerSelection = getComputerChoice()
+const playerSelection = getPlayerChoice()
+
 
 
 
 function getComputerChoice(){
-    computerSelection = possibleChoises[Math.floor(Math.random() * possibleChoises.length)]
-    console.log('Computer Selection: ', computerSelection)
+    computerSelection = possibleChoices[Math.floor(Math.random() * possibleChoices.length)]
+    console.log("Computer Selection: ", computerSelection)
+
+
 
 }
+
+
+function getPlayerChoice() {
+    playerSelection = prompt("Write one of these: Rock, Paper or Scissors").toLowerCase()
+    
+    return getPlayerChoice();
+
+}
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection)
+    {
+        return "It's a tie";
+    }
+
+    if (playerSelection === 'rock')
+    {
+        if (computerSelection === 'scissors')
+        {
+            console.log("Player Win")
+            return "Player win";
+        }
+        else 
+        {
+            console.log("Computer Win")
+            return "Computer Win";
+        }
+    }
+
+    if (playerSelection === 'paper')
+    {
+        if (computerSelection === 'rock')
+        {
+            console.log("Player Win")
+            return "Player Win";
+        }
+        else 
+        {
+            console.log("Computer Win")
+            return "Computer Win";
+        }
+    }
+
+    if (playerSelection === 'scissors')
+    {
+        if (computerSelection === 'paper')
+        {
+            console.log("Player Win")
+            return "Player Win";
+        }
+        else
+        {
+            console.log("Computer Win")
+            return "Computer Win";
+        }
+    }
+    
+
+
+}
+
+
 
 
