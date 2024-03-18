c8a3e9dec3b8116d1096bcc1373483b9e6dfd4db
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-03-18 13:26:36 +0100

    Add playGame function and include some return statements in the other functions

--- script.js
+++ script.js
@@ -2,12 +2,50 @@ const possibleChoices = ['rock', 'paper', 'scissors']
 const computerSelection = getComputerChoice()
 const playerSelection = getPlayerChoice()
 
+function playGame(){
+
+    player_score = 0;
+    computer_score = 0;
+
+    for (let i = 0; i < 6; i++){
+        console.log(`Round number ${i}`)
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection)
+
+        if (result === "Player Win")
+        {
+            player_score += 1;
+        }
+        else if (result === "Computer Win")
+        {
+            computer_score += 1;
+        }
+    }
+    console.log(`Player Score: ${player_score}`);
+    console.log(`Computer Score: ${computer_score}`);
+
+    if (player_score > computer_score)
+    {
+        console.log("Player wins!!!!")
+    }
+    else if (player_score === computer_score)
+    {
+        console.log("It's a tie")
+    }
+    else
+    {
+        console.log("Computer wins!!!")
+    }
+}
+
 
 
 
 function getComputerChoice(){
     computerSelection = possibleChoices[Math.floor(Math.random() * possibleChoices.length)]
     console.log("Computer Selection: ", computerSelection)
+    return computerSelection;
 
 
 
@@ -16,10 +54,10 @@ function getComputerChoice(){
 
 function getPlayerChoice() {
     playerSelection = prompt("Write one of these: Rock, Paper or Scissors").toLowerCase()
+    return playerSelection;
     
-    return getPlayerChoice();
-
 }
+
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection)
@@ -32,7 +70,7 @@ function playRound(playerSelection, computerSelection){
         if (computerSelection === 'scissors')
         {
             console.log("Player Win")
-            return "Player win";
+            return "Player Win";
         }
         else 
         {
@@ -68,8 +106,6 @@ function playRound(playerSelection, computerSelection){
             return "Computer Win";
         }
     }
-    
-
 
 }
 
